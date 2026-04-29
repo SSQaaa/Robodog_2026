75461e3002beecd1e5aad4dc4ac4d92eec86fde2
SSQaaa <24061205@hdu.edu.cn>
2026-04-29 21:22:37 +0800

    添加是否rotate180度开关以及添加获取相机内参函数

--- bindings.cpp
+++ bindings.cpp
@@ -13,6 +13,9 @@ PYBIND11_MODULE(orbbec_native, m) {
         .def("get_color_frame", &OrbbecCamera::get_color_frame)
         .def("get_color_size", &OrbbecCamera::get_color_size)
         .def("get_depth_size", &OrbbecCamera::get_depth_size)
+        .def("get_color_intrinsics", &OrbbecCamera::get_color_intrinsics)
+        .def("get_depth_intrinsics", &OrbbecCamera::get_depth_intrinsics)
+        .def("is_rotate_180_enabled", &OrbbecCamera::is_rotate_180_enabled)
         .def("get_depth", &OrbbecCamera::get_depth)
         .def("get_depth_in_box", &OrbbecCamera::get_depth_in_box);
 }
